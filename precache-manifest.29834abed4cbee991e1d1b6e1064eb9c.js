6c7fa966cb09852193de810f4705c07059ae26ee
jayrahida <jayrahida@gmail.com>
2018-11-04 02:27:05 +1100

    Updates

---
+++ precache-manifest.29834abed4cbee991e1d1b6e1064eb9c.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b813c2ae0d04909a33a18d792912ee7",
+    "url": "/react/static/media/Roboto-Light.3b813c2a.woff"
+  },
+  {
+    "revision": "209318a6cdab2cca60eb",
+    "url": "/react/static/css/main.13f71d8b.chunk.css"
+  },
+  {
+    "revision": "e31fcf1885e371e19f5786c2bdfeae1b",
+    "url": "/react/static/media/Roboto-Bold.e31fcf18.ttf"
+  },
+  {
+    "revision": "2db0bf171a3df933955e",
+    "url": "/react/static/js/1.2db0bf17.chunk.js"
+  },
+  {
+    "revision": "bf2515a31126aef78a60",
+    "url": "/react/static/js/runtime~main.bf2515a3.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "dfe56a876d0282555d1e2458e278060f",
+    "url": "/react/static/media/Roboto-Thin.dfe56a87.eot"
+  },
+  {
+    "revision": "954bbdeb86483e4ffea00c4591530ece",
+    "url": "/react/static/media/Roboto-Thin.954bbdeb.woff2"
+  },
+  {
+    "revision": "7500519de3d82e33d1587f8042e2afcb",
+    "url": "/react/static/media/Roboto-Thin.7500519d.woff"
+  },
+  {
+    "revision": "a990f611f2305dc12965f186c2ef2690",
+    "url": "/react/static/media/Roboto-Light.a990f611.eot"
+  },
+  {
+    "revision": "69f8a0617ac472f78e45841323a3df9e",
+    "url": "/react/static/media/Roboto-Light.69f8a061.woff2"
+  },
+  {
+    "revision": "94998475f6aea65f558494802416c1cf",
+    "url": "/react/static/media/Roboto-Thin.94998475.ttf"
+  },
+  {
+    "revision": "209318a6cdab2cca60eb",
+    "url": "/react/static/js/main.209318a6.chunk.js"
+  },
+  {
+    "revision": "30799efa5bf74129468ad4e257551dc3",
+    "url": "/react/static/media/Roboto-Regular.30799efa.eot"
+  },
+  {
+    "revision": "2751ee43015f9884c3642f103b7f70c9",
+    "url": "/react/static/media/Roboto-Regular.2751ee43.woff2"
+  },
+  {
+    "revision": "ba3dcd8903e3d0af5de7792777f8ae0d",
+    "url": "/react/static/media/Roboto-Regular.ba3dcd89.woff"
+  },
+  {
+    "revision": "4d9f3f9e5195e7b074bb63ba4ce42208",
+    "url": "/react/static/media/Roboto-Medium.4d9f3f9e.eot"
+  },
+  {
+    "revision": "46e48ce0628835f68a7369d0254e4283",
+    "url": "/react/static/media/Roboto-Light.46e48ce0.ttf"
+  },
+  {
+    "revision": "574fd0b50367f886d359e8264938fc37",
+    "url": "/react/static/media/Roboto-Medium.574fd0b5.woff2"
+  },
+  {
+    "revision": "df7b648ce5356ea1ebce435b3459fd60",
+    "url": "/react/static/media/Roboto-Regular.df7b648c.ttf"
+  },
+  {
+    "revision": "fc78759e93a6cac50458610e3d9d63a0",
+    "url": "/react/static/media/Roboto-Medium.fc78759e.woff"
+  },
+  {
+    "revision": "ecdd509cadbf1ea78b8d2e31ec52328c",
+    "url": "/react/static/media/Roboto-Bold.ecdd509c.eot"
+  },
+  {
+    "revision": "39b2c3031be6b4ea96e2e3e95d307814",
+    "url": "/react/static/media/Roboto-Bold.39b2c303.woff2"
+  },
+  {
+    "revision": "dc81817def276b4f21395f7ea5e88dcd",
+    "url": "/react/static/media/Roboto-Bold.dc81817d.woff"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/react/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "894a2ede85a483bf9bedefd4db45cdb9",
+    "url": "/react/static/media/Roboto-Medium.894a2ede.ttf"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/react/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "2db0bf171a3df933955e",
+    "url": "/react/static/css/1.1d7e54a0.chunk.css"
+  },
+  {
+    "revision": "cc36653212585f6efb4ec64ebf008b43",
+    "url": "/react/index.html"
+  }
+];
